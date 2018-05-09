48d9a0a2c11f51421a8fe23a18a71d7e90613a3e
walle <assqmyyyrhcjcx@aliyun.com>
2018-05-09 20:11:32 +0800

    update page

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -9,7 +9,7 @@
         <div class="filter-nav">
           <span class="sortby">Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
-          <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+          <a href="javascript:void(0)" class="price" @click="sortGoods()">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
           <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop()">Filter by</a>
         </div>
         <div class="accessory-result">
@@ -34,7 +34,7 @@
                   </div>
                   <div class="main">
                     <div class="name">{{item.productName}}</div>
-                    <div class="price">{{item.productPrice}}</div>
+                    <div class="price">{{item.salePrice}}</div>
                     <div class="btn-area">
                       <a href="javascript:;" class="btn btn--m">加入购物车</a>
                     </div>
@@ -43,6 +43,9 @@
               </ul>
             </div>
           </div>
+        </div>
+        <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="30">
+          ...
         </div>
       </div>
     </div>
@@ -78,10 +81,13 @@
             endPrice: 1999
           }
         ],
+        page: 1,
+        pageSize: 8,
         priceChecked: "all",
         filterBy: false,
         overLayFlag: false,
-        sortFlag: true
+        sortFlag: true,
+        busy: true
       }
     },
     components: {
@@ -118,7 +124,13 @@
       sortGoods() {
         this.sortFlag = !this.sortFlag;
         this.page = 1;
-        this.sortGoods();
+        this.getGoodsList();
+      },
+      loadMore() {
+        setTimeout(() => {
+          this.busy = true;
+          this.busy = false;
+        }, 3000);
       }
     }
   }
